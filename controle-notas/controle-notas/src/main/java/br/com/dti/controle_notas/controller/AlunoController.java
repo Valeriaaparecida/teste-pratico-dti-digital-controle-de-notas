0199dd14aaaea50fe6ffe92c8f3c76bb32b87c2d
Valeria Aparecida <valeria777valeria@hotmail.com>
2026-02-06 08:55:28 -0300

    chore(docker): adiciona docker-compose do postgres para desenvolvimento local

--- controle-notas/controle-notas/src/main/java/br/com/dti/controle_notas/controller/AlunoController.java
+++ controle-notas/controle-notas/src/main/java/br/com/dti/controle_notas/controller/AlunoController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5174")
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("aluno")
 public class AlunoController {
